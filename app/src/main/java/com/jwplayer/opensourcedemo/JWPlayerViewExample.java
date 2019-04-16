9fefb394fdcd82213318e35179387ba00636eb21
hyunjkim <hkim6501@gmail.com>
2019-04-16 15:04:12 -0400

    Ad test buttons

--- app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
+++ app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -16,17 +19,16 @@ import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
 import com.jwplayer.opensourcedemo.jwutil.Logger;
 import com.jwplayer.opensourcedemo.listeners.JWAdEventHandler;
-import com.jwplayer.opensourcedemo.listeners.JWEventHandler;
 import com.jwplayer.opensourcedemo.listeners.KeepScreenOnHandler;
 import com.longtailvideo.jwplayer.JWPlayerView;
-import com.longtailvideo.jwplayer.configuration.PlayerConfig;
-import com.longtailvideo.jwplayer.configuration.SkinConfig;
+import com.longtailvideo.jwplayer.core.PlayerState;
 import com.longtailvideo.jwplayer.events.FullscreenEvent;
 import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
-import com.longtailvideo.jwplayer.media.ads.Advertising;
-import com.longtailvideo.jwplayer.media.ads.ImaAdvertising;
+import com.longtailvideo.jwplayer.media.ads.AdBreak;
+import com.longtailvideo.jwplayer.media.ads.AdSource;
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JWPlayerViewExample extends AppCompatActivity implements
@@ -48,6 +50,10 @@ public class JWPlayerViewExample extends AppCompatActivity implements
      */
     private CoordinatorLayout mCoordinatorLayout;
 
+    private String ima = "PLAY IMA";
+    private String vast = "PLAY VAST";
+    private Button adButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,10 +64,31 @@ public class JWPlayerViewExample extends AppCompatActivity implements
         mPlayerView = findViewById(R.id.jwplayer);
         TextView outputTextView = findViewById(R.id.output);
         ScrollView scrollView = findViewById(R.id.scroll);
+        adButton = findViewById(R.id.ad_button);
         mCoordinatorLayout = findViewById(R.id.activity_jwplayerview);
 
+
+        // If the player is not null
+        // and the player state is playing, idle, or buffering
+        // Load the new Ad
+        adButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mPlayerView.getState().equals(PlayerState.COMPLETE)) {
+                    mPlayerView.pause();
+                }
+                if (adButton.getText().equals(ima)) {
+                    playAd("");
+                    adButton.setText(vast);
+                } else {
+                    mPlayerView.playAd(AdSource.VAST, "https://playertest.longtailvideo.com/vpaid2-jwp-30s.xml");
+                    adButton.setText(ima);
+                }
+            }
+        });
+
         // Setup JWPlayer
-        setupJWPlayer();
+        playAd(null);
 
         // Handle hiding/showing of ActionBar
         mPlayerView.addOnFullscreenListener(this);
@@ -70,7 +97,7 @@ public class JWPlayerViewExample extends AppCompatActivity implements
         new KeepScreenOnHandler(mPlayerView, getWindow());
 
         // Instantiate the JW Player event handler class
-        new JWEventHandler(mPlayerView, outputTextView, scrollView);
+//        new JWEventHandler(mPlayerView, outputTextView, scrollView);
 
         // Instantiate the JW Player Ad event handler class
         new JWAdEventHandler(mPlayerView, outputTextView, scrollView);
@@ -78,45 +105,20 @@ public class JWPlayerViewExample extends AppCompatActivity implements
         mCastContext = CastContext.getSharedInstance(this);
     }
 
-    /* Setup JW Player
-     * More info about our Player Configuration and other available Configurations: {@link - https://developer.jwplayer.com/sdk/android/reference/com/longtailvideo/jwplayer/configuration/package-summary.html}
-     * 1 - PlayerConfig
-     * 2 - LogoConfig
-     * 3 - PlaybackRateConfig
-     * 4 - CaptionsConfig
-     * 5 - RelatedConfig
-     * 6 - SharingConfig
-     * 7 - SkinConfig
-     */
-    private void setupJWPlayer() {
-
-        List<PlaylistItem> playlistItemList = SamplePlaylist.createPlaylist();
-//		List<PlaylistItem> playlistItemList = SamplePlaylist.createMediaSourcePlaylist();
-
-        // Ima Tag Example
-        ImaAdvertising imaAdvertising = SampleAds.getImaAd();
-
-        // VAST Tag Example
-        Advertising vastAdvertising = SampleAds.getVastAd();
-
-        // SkinConifg - more info: https://developer.jwplayer.com/sdk/android/reference/com/longtailvideo/jwplayer/configuration/SkinConfig.Builder.html
-        SkinConfig skinConfig = new SkinConfig.Builder()
-                .url("https://www.host.com/css/mycustomcss.css")
-                .name("mycustomcss")
-                .build();
-
-        // More info: https://developer.jwplayer.com/sdk/android/reference/com/longtailvideo/jwplayer/configuration/PlayerConfig.Builder.html
-        PlayerConfig config = new PlayerConfig.Builder()
-                .playlist(playlistItemList)
-                .autostart(true)
-                .preload(true)
-                .allowCrossProtocolRedirects(true)
-//				.advertising(vastAdvertising)
-//				.advertising(imaAdvertising)
-                .skinConfig(skinConfig)
-                .build();
-
-        mPlayerView.setup(config);
+    private void playAd(String client) {
+        PlaylistItem item = new PlaylistItem("https://cdn.jwplayer.com/manifests/jumBvHdL.m3u8");
+        if (client != null) {
+            String ad = "https://playertest.longtailvideo.com/vpaid2-jwp-30s.xml";
+            AdBreak adBreak = adButton.getText().equals(ima) ? new AdBreak("pre", AdSource.IMA, ad): new AdBreak("pre", AdSource.VAST, ad);
+            List<AdBreak> adSchedule = new ArrayList<AdBreak>() {
+                {
+                    add(adBreak);
+                }
+            };
+            item.setAdSchedule(adSchedule);
+        }
+        mPlayerView.load(item);
+        mPlayerView.play();
     }
 
 
