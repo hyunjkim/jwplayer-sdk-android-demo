69718b64eeeacdbabb14c0bd45b9446922aeac2a
hyunjkim <hkim6501@gmail.com>
2019-11-22 17:54:34 -0500

    Sample with modular examples

---
+++ FullscreenHandlerDemo/src/main/java/com/example/jwplayersdk/fullscreenhandlerdemo/JWPlayerViewExample.java
@@ -0,0 +1,163 @@
+package com.example.jwplayersdk.fullscreenhandlerdemo;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jwplayersdk.mediasourcedemo.R;
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.events.FullscreenEvent;
+import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
+import com.longtailvideo.jwplayer.media.playlists.MediaSource;
+import com.longtailvideo.jwplayer.media.playlists.MediaType;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JWPlayerViewExample extends AppCompatActivity
+        implements VideoPlayerEvents.OnFullscreenListener {
+
+    private JWPlayerView mPlayerView;
+
+    private CastContext mCastContext;
+
+    private CallbackScreen mCallbackScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jwplayerview);
+
+        mPlayerView = findViewById(R.id.jwplayer);
+
+
+        // Handle hiding/showing of ActionBar
+        mPlayerView.addOnFullscreenListener(this);
+
+        // Keep the screen on during playback
+        new KeepScreenOnHandler(mPlayerView, getWindow());
+
+        // Event Logging
+        mCallbackScreen = findViewById(R.id.callback_screen);
+        mCallbackScreen.registerListeners(mPlayerView);
+
+
+        String url =
+                "https://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8";
+//                "http://93.190.141.15:7004/humm_64.mp3?userToken=h2TJgv6ayQW1v9HBDOzDcf8";
+
+        List<MediaSource> mslist = new ArrayList<MediaSource>() {{
+            add(new MediaSource(url, "mp3", false, MediaType.MP3, null));
+        }};
+
+        // Load a media source
+        PlaylistItem pi = new PlaylistItem.Builder()
+                .file(url)
+//                .sources(mslist)
+                .build();
+
+        mPlayerView.load(pi);
+
+        // Get a reference to the CastContext
+        mCastContext = CastContext.getSharedInstance(this);
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPlayerView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPlayerView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPlayerView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPlayerView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPlayerView.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // Set fullscreen when the device is rotated to landscape
+        mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE,
+                true);
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Exit fullscreen when the user pressed the Back button
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mPlayerView.getFullscreen()) {
+                mPlayerView.setFullscreen(false, true);
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onFullscreen(FullscreenEvent fullscreenEvent) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            if (fullscreenEvent.getFullscreen()) {
+                actionBar.hide();
+            } else {
+                actionBar.show();
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_jwplayerview, menu);
+
+        // Register the MediaRouterButton
+        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
+                R.id.media_route_menu_item);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.switch_to_fragment:
+                Intent i = new Intent(this, JWPlayerFragmentExample.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
