c92c3c22d1d7c13dcd11fd1806acb16eb2360662
hyunjkim <hkim6501@gmail.com>
2020-01-30 11:49:21 -0500

    updated android sdk version and changed playlist media files

--- app/src/main/java/com/jwplayer/opensourcedemo/SamplePlaylist.java
+++ app/src/main/java/com/jwplayer/opensourcedemo/SamplePlaylist.java
@@ -18,18 +18,14 @@ public class SamplePlaylist {
         List<PlaylistItem> playlistItemList = new ArrayList<>();
 
         String[] playlist = {
-                "https://cdn.jwplayer.com/manifests/RDn7eg0o.m3u8",
-                "http://content.jwplatform.com/videos/tkM1zvBq-cIp6U8lV.mp4",
-                "https://cdn.jwplayer.com/manifests/8TbJTFy5.m3u8",
-                "https://content.jwplatform.com/videos/i3q4gcBi-cIp6U8lV.mp4",
-                "http://content.jwplatform.com/videos/iLwfYW2S-cIp6U8lV.mp4"
+                "https://cdn.jwplayer.com/manifests/3HIGGMiI.m3u8",
+                "https://cdn.jwplayer.com/manifests/DHfpVnJB.m3u8",
+                "https://cdn.jwplayer.com/manifests/o89p07oM.m3u8"
         };
         String[] images = {
-                "RDn7eg0o",
-                "tkM1zvBq",
-                "8TbJTFy5",
-                "i3q4gcBi",
-                "iLwfYW2S"
+                "3HIGGMiI",
+                "DHfpVnJB",
+                "o89p07oM",
         };
 
         int index = 0;
@@ -54,55 +50,6 @@ public class SamplePlaylist {
 
             playlistItemList.add(item);
         }
-
-        return playlistItemList;
-    }
-
-    /**
-     * MediaSource Playlist Example
-     * <p>
-     * Adaptive Streaming for more info: https://support.jwplayer.com/articles/adaptive-streaming-reference
-     */
-    public static List<PlaylistItem> createMediaSourcePlaylist() {
-        List<MediaSource> mediaSourceList = new ArrayList<>();
-        List<PlaylistItem> playlistItemList = new ArrayList<>();
-
-        String hls = "https://cdn.jwplayer.com/manifests/RDn7eg0o.m3u8";
-        String mp4 = "https://cdn.jwplayer.com/videos/RDn7eg0o-Zq6530MP.mp4";
-        String mp4_1 = "https://cdn.jwplayer.com/videos/RDn7eg0o-TNpruJId.mp4";
-        String mp4_2 = "https://cdn.jwplayer.com/videos/RDn7eg0o-FctPAkow.mp4";
-
-        MediaSource mshls = new MediaSource.Builder()
-                .file(hls)
-                .label("HLS")
-                .type(MediaType.HLS)
-                .build();
-        MediaSource ms = new MediaSource.Builder()
-                .file(mp4)
-                .label("180px")
-                .type(MediaType.MP4)
-                .build();
-        MediaSource ms1 = new MediaSource.Builder()
-                .file(mp4_1)
-                .label("480px")
-                .type(MediaType.MP4)
-                .build();
-        MediaSource ms2 = new MediaSource.Builder()
-                .file(mp4_2)
-                .label("1280px")
-                .type(MediaType.MP4)
-                .build();
-
-        mediaSourceList.add(ms);
-        mediaSourceList.add(ms1);
-        mediaSourceList.add(ms2);
-        mediaSourceList.add(mshls);
-
-        PlaylistItem item = new PlaylistItem.Builder()
-                .sources(mediaSourceList)
-                .build();
-
-        playlistItemList.add(item);
 
         return playlistItemList;
     }
