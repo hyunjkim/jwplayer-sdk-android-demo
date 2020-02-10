08c8462c34d293cc84ec197178862b1cd415ae71
hyunjkim <hkim6501@gmail.com>
2020-02-10 14:45:48 -0500

    stretch example

--- app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
+++ app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
@@ -3,11 +3,13 @@ package com.jwplayer.opensourcedemo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.longtailvideo.jwplayer.JWPlayerView;
-import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+import com.longtailvideo.jwplayer.configuration.PlayerConfig;
 
 
 public class JWPlayerViewExample extends AppCompatActivity {
@@ -21,21 +23,63 @@ public class JWPlayerViewExample extends AppCompatActivity {
 
         mPlayerView = findViewById(R.id.jwplayer);
 
-        // Load a media source
-        PlaylistItem pi = new PlaylistItem.Builder()
-                .file("https://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
-                .title("BipBop")
-                .description("A video player testing video.")
+        // Stretching None
+        findViewById(R.id.none).setOnClickListener(v -> {
+            if (mPlayerView.getConfig() != null) {
+                setupStretching(PlayerConfig.STRETCHING_NONE);
+            }
+        });
+        // Stretching Exact fit
+        findViewById(R.id.fit).setOnClickListener(v -> {
+            if (mPlayerView.getConfig() != null) {
+                setupStretching(PlayerConfig.STRETCHING_EXACT_FIT);
+            }
+        });
+        // Stretching Fill
+        findViewById(R.id.fill).setOnClickListener(v -> {
+            if (mPlayerView.getConfig() != null) {
+                setupStretching(PlayerConfig.STRETCHING_FILL);
+            }
+        });
+        // Stretching Uniform
+        findViewById(R.id.uniform).setOnClickListener(v -> {
+            if (mPlayerView.getConfig() != null) {
+                setupStretching(PlayerConfig.STRETCHING_UNIFORM);
+            }
+        });
+
+        String video = "https://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8";
+
+        // Build Player Config
+        PlayerConfig config = new PlayerConfig.Builder()
+                .file(video)
+                .autostart(true)
                 .build();
 
-        mPlayerView.load(pi);
-
+        // Setup JWPlayer
+        mPlayerView.setup(config);
 
         // Initialize event listeners
         CallbackScreen cbs = findViewById(R.id.callback_screen);
         cbs.registerListeners(mPlayerView);
     }
 
+    /*
+    * Setup Stretching here
+    * */
+    public void setupStretching(String stretch) {
+
+        Toast.makeText(this, stretch, Toast.LENGTH_SHORT).show();
+
+        // Get Config
+        PlayerConfig mConfig = mPlayerView.getConfig();
+
+        // Set the Stretching
+        mConfig.setStretching(stretch);
+
+        // Setup the Config
+        mPlayerView.setup(mConfig);
+    }
 
     @Override
     protected void onStart() {
